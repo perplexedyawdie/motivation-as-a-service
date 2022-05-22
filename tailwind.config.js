ed70958786bb6022f6289310506f266b3a39f03a
perplexedyawdie <sunkwenga@gmail.com>
2022-05-22 01:07:55 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "polygon": " url('/assets/polygon-scatter-haikei.svg'), linear-gradient(rgba(239, 239, 239, 1), rgba(239, 239, 239, 1))"
+      },
+    },
+  },
+  plugins: [],
+}
